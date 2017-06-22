acbab984112a78a389fb43c82aa9193b1cfda8da
Luca Lamorte <luca.lamorte@uni.lu>
2017-06-22 10:24:18 +0200

    Fix Integration

--- tester/__init__.py
+++ tester/__init__.py
@@ -1,18 +1,6 @@
 _last_capture = None
 
 
-TIMEOUT_MIN = 1000
-TIMEOUT_MAX = 1010
-TIMEOUT_STEP = 10
-
-ARF_MIN  = 15
-ARF_MAX  = 17
-ARF_STEP = .1
-
-RETRY_MIN = 1
-RETRY_MAX = 4
-
-
 IFC = "lo0"
 FILENAME = "data/mqtt_capture"
 
@@ -22,5 +10,9 @@ DATADIR = "data"
 LOGDIR = "log"
 
 
-NUM_TEST = 1
+
 TEST_RESULT= "data/report.txt"
+PAYLOAD_LIST = [128, 256, 512, 1024]
+QoS = 2
+N_PACKET_SEND = 2
+TOPIC = 'topic'
--- tester/__main__.py
+++ tester/__main__.py
@@ -1,23 +1,29 @@
 import errno
+
 from tester import *
 from tester.commands import *
-from tester.analyze import N_PACKET_SEND
+from tester import N_PACKET_SEND, PAYLOAD_LIST, TOPIC
 from tester.write_ouput import write_test_result
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-def start_mqtt_client(qos, payload):
-    global logger
+def start_mqtt_client(set_qos, payload_size):
 
-    msg = "Start MQTT CLIENT QoS=%d Payload:%d"  %(qos, payload)
-    logger.debug(msg)
+    payload = ""
+    for i in range(payload_size):
+        payload += "X"
 
-    params = 'echo %s' %msg
-    os.system(params)
+    logger.debug("Payload size: %d" % len(payload))
+
+    params = 'mosquitto_pub -q %s -t %s -m %s ' % (set_qos, TOPIC, payload)
+    logger.debug(params)
+    for count in range(1, N_PACKET_SEND):
+        os.system(params)
 
 
 
 if __name__ == '__main__':
-
 
     # generate dirs
     for d in TMPDIR, DATADIR, LOGDIR:
@@ -27,10 +33,10 @@ if __name__ == '__main__':
             if e.errno != errno.EEXIST:
                 raise
 
-    logger.debug("START TEST...")
+    logger.debug("START  MQTT TEST...")
     index = 0
     for qos in (1, 2):
-        for payload in (128, 256, 1024):
+        for payload in PAYLOAD_LIST:
 
                 file_id = ('_'.join([FILENAME,
                                     "qos",
@@ -41,11 +47,11 @@ if __name__ == '__main__':
 
                 file_name = '%s.pcap' % file_id
                 launch_sniffer(file_name, IFC, other_filter='tcp and port 1883')
-                start_mqtt_client(qos=qos, payload=payload)
+                start_mqtt_client(qos, payload)
                 stop_sniffer()
                 decode_pcap(file_name)
                 write_test_result(index, payload, file_id, N_PACKET_SEND)
 
                 index+=1
 
-    logger.debug("TEST is over!")
+    logger.debug("TEST MQTT is over!")
