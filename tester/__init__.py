c9f757fc0d7d1649bea49a7cf0a1e97c8da6cb93
Luca Lamorte <luca.lamorte@uni.lu>
2017-09-14 15:50:08 +0200

    Add conf mosquitto

--- tester/__init__.py
+++ tester/__init__.py
@@ -15,13 +15,25 @@ BACKUP = "backup"
 TEST_RESULT= "data/report.txt"
 #PAYLOAD_LIST = [128, 256, 512, 1024]
 PAYLOAD_LIST = [128]
+<<<<<<< Updated upstream
 QoS = [1, 2]
+=======
+QoS = [1]
+>>>>>>> Stashed changes
 N_PACKET_SEND = 100
 TOPIC = 'topic'
 
 
+<<<<<<< Updated upstream
 IFC = "lo0"
 GW_IP = "192.168.42.1"
 WAIT_START_TCPDUMP = 1
 WAIT_CLOSE_TCPDUMP = 2
 TCPDUMP_FILTER =  '' # keep this empty or put ICMP + TCP
+=======
+IFC = "opensand_tun"
+GW_IP = "192.168.42.1"
+WAIT_START_TCPDUMP = 1
+WAIT_CLOSE_TCPDUMP = 2
+TCPDUMP_FILTER =  '' # keep this empty or put ICMP + TCP
+>>>>>>> Stashed changes
